a087af345a854f179c7d627cd2c9952d36a95bf6
purrvax <ppsingh_b24@et.vjti.ac.in>
2026-08-02 00:23:34 +0530

    changed embedding model and notes.css

--- backend/ai/embeddings.py
+++ backend/ai/embeddings.py
@@ -1,7 +1,7 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 def get_embedding_model():
     embeddings = HuggingFaceEmbeddings(
-        model_name="BAAI/bge-base-en-v1.5",
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
         encode_kwargs={
             "normalize_embeddings": True
         }
